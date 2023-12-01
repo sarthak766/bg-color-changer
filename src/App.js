6b3f815ee8871d79befc76a14fbc37736175550a
sarthak badola <sarthakbadola97@gmail.com>
2023-12-01 12:26:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import {useState} from "react"
 import './App.css';
 
 function App() {
+   const [color,setColor]= useState("blue")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" w-full h-screen duration-200"
+    style={{backgroundColor : color}}>
+      <div className="contain">
+        <div className="modify">
+          <h3>YOUR BACKGROUND COLOR CHANGER</h3>
+           <br/>
+          <button 
+          onClick={() => setColor("white")}
+          className="btn"
+          >
+              white
+          </button>
+          <button className="btn"
+          onClick={() => setColor("black")}
+          >
+              black
+          </button>
+          <button className="btn"
+          onClick={() => setColor("olive")}>
+               olive            
+          </button>
+          <button className="btn"
+          onClick={() => setColor("violet")}>
+               violet
+          </button>
+          <button className="btn"
+          onClick={() => setColor("skyblue")}>
+               skyblue
+          </button>
+          <button className="btn"
+          onClick={() => setColor("orange")}>
+               orange
+          </button>
+        </div>
+
+      </div>
+
+       
     </div>
   );
 }
